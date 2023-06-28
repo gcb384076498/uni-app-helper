e0e82492dc04aadcd10925c610cfc0d7775617a8
GitHub Action <action@github.com>
2023-06-28 05:31:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/gcb384076498/uniapphelper/MyBundle.kt
+++ src/main/kotlin/com/github/gcb384076498/uniapphelper/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gcb384076498.uniapphelper
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gcb384076498/uniapphelper/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gcb384076498/uniapphelper/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gcb384076498.uniapphelper.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
